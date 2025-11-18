c4c6d170c90ad44b46f1c5ad4d212850e7cd15bb
Shinichi Yoshinaga <shinichi.yoshinaga@brother.co.jp>
2025-11-19 05:48:20 +0900

    up

---
+++ solutions/ContestChallenge/ContestChallenge/ContestChallenge.cpp
@@ -0,0 +1,21 @@
+﻿// ContestChallenge.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include "DynamicsPlanning.h"
+
+using namespace std;
+
+int main()
+{
+    DynamicsPlanning *dp = new DynamicsPlanning();
+    cout << "Hello World!\n";
+}
+
+void Exe1() {
+    int num = 4;
+    int w[4] = { 2, 2, 3, 2 }, v[4] = {3, 2, 4, 2};
+    int LimitW = 5;
+
+}
+
---
+++ solutions/ContestChallenge/ContestChallenge/DynamicsPlanning.h
@@ -0,0 +1,9 @@
+#pragma once
+class DynamicsPlanning
+{
+public:
+	DynamicsPlanning();
+private:
+	
+};
+
