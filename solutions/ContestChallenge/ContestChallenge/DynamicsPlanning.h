3d05962c5d2b5b431f9e11253e1f7439afd48c62
Shinichi Yoshinaga <yoshishinnze@gmail.com>
2025-12-23 05:33:47 +0900

    up

--- solutions/ContestChallenge/ContestChallenge/DynamicsPlanning.h
+++ solutions/ContestChallenge/ContestChallenge/DynamicsPlanning.h
@@ -6,6 +6,7 @@ public:
 	int Drawing();
 	int solveFibonacciRecursive();
 	int search_napsack(int* weight, int* value, int LimitW, int num);
+	int searchDynamicMemo(int* weight, int* value, int LimitW, int numLimit);
 private:
 	
 };
